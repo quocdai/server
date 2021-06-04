152e342208fa64f82ef971c1ce7d0b790c749d94
Cafe Game <quocdai.online@gmail.com>
2021-06-04 11:02:07 +0700

    clear

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from "@nestjs/mongoose";
-import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/products-docker', {
-      useNewUrlParser: true
-    }),
-    ProductModule
-  ],
+  imports: [], // [MongooseModule.forRoot(process.env.DB_URL)],
   controllers: [AppController],
   providers: [AppService],
 })
